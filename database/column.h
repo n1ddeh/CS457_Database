30012ee68860bed55fc0c9a91489bfacd7ddeb58
Mark Minkoff <markminkoff@protonmail.com>
2021-03-23 09:51:38 -0700

    🎭 Comments in column.h

--- database/column.h
+++ database/column.h
@@ -1,3 +1,10 @@
+/**
+ * File: column.h
+ * Author: Mark Minkoff
+ * Functionality: Function declarations for file column.cpp
+ * 
+ * */
+
 #ifndef COLUMN_H_
 #define COLUMN_H_
 
@@ -13,19 +20,44 @@ private:
     size_t CHAR_MAX;                // Used for VARCHAR types
 
 public:
+    // ---------------------------
+    // ---- Constructors
+    // ---------------------------
+
+    // Constructor for Column<int>
     Column(std::string, std::vector<int>);
+
+    // Constructor for Column<float>
     Column(std::string, std::vector<float>);
+
+    // Constructor for Column<float>
     Column(std::string, std::vector<char>);
+
+    // Constructor for Column<float>
     Column(std::string, std::vector<std::string>, size_t);
 
+    // ---------------------------
+    // ---- Mutator Functions
+    // ---------------------------
+
+    // Inserts an element
     bool insertElement(T);
+
+    // Deletes an element at some specified row
     bool deleteElement(const size_t);
+
+    // ---------------------------
+    // ---- Getter Functions
+    // ---------------------------
 
     std::string getName() {return this->column_name;}
     unsigned int getDataType() {return this->data_type;}
     std::vector<T> getElements() {return this->elements;}
     size_t getCharMax() {return this->CHAR_MAX;}
 
+    // ---------------------------
+    // ---- Helper Functions
+    // ---------------------------
 
     std::unordered_set<size_t> filterElements(const std::string& op, T val);
     size_t updateElementsOnIndex(const std::unordered_set<size_t>&, const T&);
